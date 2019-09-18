1d5f1479b3d372d7b40e6ccf5a465fd535a47c7e
George McCabe <mccabe@huni.rap.ucar.edu>
2019-09-18 21:37:57 +0000

    Read print_params output and substitute values from parsed input, support doZip and verificationEmail, fixed various bugs

--- helper/Archiver2GA.py
+++ helper/Archiver2GA.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 
 import sys
+import os
+import re
+from subprocess import Popen, PIPE
 
 def subDateStrings(s):
   #print(s)
+  s = s.replace("DATEYYYYMMDD","%Y%m%d")
   s = s.replace("DATEYYYY","%Y")
   s = s.replace("DATEJJJ","%j")
   s = s.replace("DATEMMDD","%m%d")
-  s = s.replace("DATEYYYYMMDD","%Y%m%d")
   #print(s)
   return s
 
+def getDefaultParam():
+  globus_script = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'GlobusArchiver.py')
+  process = Popen([globus_script, '--print_params'], stdout=PIPE)
+  (output, err) = process.communicate()
+  return output.decode('utf-8')
 
 def main():
-   
+
   input_file = sys.argv[1]
 
+  program = None
+  if len(sys.argv) > 2:
+    program = sys.argv[2]
 
   #archiveItems = {
   #  "hrrr-ak-wrfnat-data":
@@ -40,55 +51,108 @@ def main():
   #          <destination>/RAPDMG/SATELLITE/GOES16/ABI/L2/FULL_DISK/DATEYYYY/DATEYYYYDATEJJJ/</destination>
 
   #          <tarFilename>OR_ABI-L2-FULL_DISK.VAA.DATEYYYYDATEJJJ.tar</tarFilename>
-            
-            
 
-      
+  defaultParam = getDefaultParam()
+
+  output = ''
+  indent = 4
+
   with open(input_file, "r") as in_file:
+    # item counter
     item = 0
+    # keep track if we are inside an archiveItem
+    inItem = False
+    # if doZip is set for all items, set it for each unless that item has doZip = false
+    doZipAll = False
+    # get email address
+    verificationEmail = None
+
     for line in in_file:
 
+      # replace /RAPDMG with the new path to the RAPDMG area
+      if program and '/RAPDMG/projects' in line:
+        line = line.replace('/RAPDMG/projects', f'/gpfs/csfs1/ral/{program}')
+
+      # get email address
+      if "<verificationEmail>" in line:
+        verificationEmail = line.replace('<verificationEmail>', '').replace('</verificationEmail>', '').rstrip()
+
       if "<archiveItem>" in line:
-            print(f'"item-{item}:"')
-            print("{")
+            output += f'"item-{item}":\n'
+            output += ' ' * indent + "{\n"
+            inItem = True
+            # keep track if doZip was set in this archiveItem so we know to override
+            # it with global doZip or not
+            doZipIsSet = False
 
       if "<source>" in line:
         source = subDateStrings(line.replace("<source>","").replace("</source>","")).rstrip()
-        print(f'"source": "{source}"')
+        output += ' ' * indent + f'"source": "{source}",\n'
 
       if "<destination>" in line:
         destination = subDateStrings(line.replace("<destination>","").replace("</destination>","")).rstrip()
-        print(f'"destination": "{destination}"')
+        output += ' ' * indent + f'"destination": "{destination}",\n'
 
-      if "<tarFilename" in line:
+      if "<tarFilename>" in line:
         tarFilename = subDateStrings(line.replace("<tarFilename>","").replace("</tarFilename>","")).rstrip()
-        print(f'"tarFileName": "{tarFilename}"')
+        output += ' ' * indent + f'"tarFileName": "{tarFilename}",\n'
 
       if "<cdDirTar>" in line:
         cdDirTar = subDateStrings(line.replace("<cdDirTar>","").replace("</cdDirTar>","")).rstrip()
-        print(f'"cdDirTar": "{cdDirTar}"')
+        output += ' ' * indent + f'"cdDirTar": "{cdDirTar}",\n'
 
       if "<expectedNumFiles>" in line:
         expectedNumFiles = line.replace("<expectedNumFiles>","").replace("</expectedNumFiles>","").rstrip()
-        print(f'"expectedNumFiles": {expectedNumFiles}')
+        output += ' ' * indent + f'"expectedNumFiles": {expectedNumFiles},\n'
 
       if "<expectedFileSize>" in line:
         expectedFileSize = line.replace("<expectedFileSize>","").replace("</expectedFileSize>","").rstrip()
-        print(f'"expectedFileSize": {expectedFileSize}')
+        output += ' ' * indent + f'"expectedFileSize": {expectedFileSize},\n'
 
+      if "<doZip>" in line:
+        doZip = line.replace("<doZip>","").replace("</doZip>","").rstrip()
+        doZip = False if doZip.lower() == 'false' else True
 
-       
-        
+        # if outside archiveItem, this line is global doZip
+        if not inItem:
+          doZipAll = doZip
+        else:
+          output += ' ' * indent + f'"doZip": {doZip},\n'
+          doZipIsSet = True
 
       if "</archiveItem>" in line:
-            print("}")
-            item += 1
+            # if doZip was not set in this item but it was set to for all, set doZip to global doZip
+            if not doZipIsSet and doZipAll:
+              output += ' ' * indent + f'"doZip": {doZipAll},\n'
 
-      
-            
-          
-    
-        
+            output += ' ' * indent + "},\n"
+            item += 1
+            # reset booleans that pertain to a given archiveItem
+            inItem = False
+            doZipIsSet = False
+
+  # replace default values for archiveItems with converted items from input file
+  match = re.match(r'.*archiveItems\s=\s\{(.*)\}\n.*', defaultParam, re.DOTALL)
+  if not match:
+    print('NOTE: Could not parse default param file. Output is only archiveItems contents')
+    print(output)
+    exit(0)
+
+  defaultParam = defaultParam.replace(match.group(1), '\n'+output)
+
+  # replace emailAddresses line if verificationEmail was found
+  if verificationEmail is not None:
+    match = re.match(r'.*emailAddresses\s=\s\[([^]]*)\].*', defaultParam, re.DOTALL)
+    if match:
+      users = verificationEmail.split(',')
+      email_list = []
+      for user in users:
+        (username, domain) = user.strip().split('@')
+        email_list.append(f'("{user}", "{username}", "{domain}")')
+      defaultParam = defaultParam.replace(match.group(1), ','.join(email_list))
+
+  print(defaultParam)
+
 if __name__ == "__main__":
     main()
 
