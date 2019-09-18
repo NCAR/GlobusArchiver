1bfb4a05e024002fe704a842eb0661344b597028
George McCabe <mccabe@huni.rap.ucar.edu>
2019-09-18 21:37:44 +0000

    Changed default value of cleanTemp to False to files will not be deleted before they are sent

--- GlobusArchiver.py
+++ GlobusArchiver.py
@@ -129,7 +129,9 @@ archiveDateTimeString=""
 archiveDateTimeFormats=["%Y%m%d","%Y%m%d%H","%Y-%m-%dT%H:%M:%SZ"]
 
 # You may want to keep the tmp area around for debugging
-cleanTemp = True
+# If this is set to True, the data will be scrubbed before
+# the archiver can send it
+cleanTemp = False
 
 ####################################
 ## ARCHIVE ITEM CONFIGURATION
