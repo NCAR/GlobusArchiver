36d4f95a5f6c6def335778de1629f110722fa0b8
George McCabe <mccabe@nikara.rap.ucar.edu>
2019-10-22 16:00:29 +0000

    catch return code of subprocess call in run_cmd and report error if failure. Report errors inside prepare_and_transfer function and don't report based on return value of that function

--- GlobusArchiver.py
+++ GlobusArchiver.py
@@ -334,15 +334,21 @@ def run_cmd(cmd):
     # https://stackoverflow.com/questions/295459/how-do-i-use-subprocess-popen-to-connect-multiple-processes-by-pipes
     try:
         if '|' in cmd or ';' in cmd or '*' in cmd or '?' in cmd:
-            return subprocess.run(cmd, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            cmd_out = subprocess.run(cmd, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                   encoding='utf-8')
         else:
             splitcmd = shlex.split(cmd)
-            return subprocess.run(splitcmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            cmd_out = subprocess.run(splitcmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                   encoding='utf-8')
 
+        if cmd_out.returncode != 0:
+            log_and_email(f'Command returned non-zero exit status: {cmd}.', logging.error)
+            return None
+
+        return cmd_out
+
     except subprocess.CalledProcessError:
-        logging.error(f'Command returned non-zero exit status: {cmd}.')
+        log_and_email(f'Command returned non-zero exit status: {cmd}.', logging.error)
         return None
 
 
@@ -619,14 +625,12 @@ def do_transfers(transfer):
                 else:
                     ii["last_glob"] = True
                 if not prepare_and_add_transfer(transfer, tdata, ii):
-                    log_and_email(f"Prepare/Add of transfer item failed.", logging.error)
                     continue
         else:
             if not ii["glob"] and not os.path.exists(ii["source"]):
                 log_and_email(f"{ii['source']} does not exist. Skipping this archive item.", logging.error)
                 continue
             if not prepare_and_add_transfer(transfer, tdata, ii):
-                log_and_email(f"Preparation/add of transfer item failed.", logging.error)
                 continue
 
     # submit all tasks for transfer
@@ -689,9 +693,11 @@ def prepare_transfer(ii):
         cmd += "-S .gz ";  # force .gz suffix in case of differing gzip version
         cmd += ii['source'];
         logging.debug(f"ZIPing file via cmd: {cmd}")
-        # TODO: Checking for no run_cmd is not enough?    I think we are stil getting an object with stdout/err even if the cmd failed.  Need to check return code.
-        if not run_cmd(cmd):
+
+        cmd_out = run_cmd(cmd)
+        if cmd_out is None:
             return False
+
         if os.path.isfile(ii['source']):
             ii['source'] += ".gz"
 
@@ -718,15 +724,19 @@ def prepare_transfer(ii):
         if ii.get("skipUnderscoreFiles"):
             cmd += " --exclude \"_*\""
 
-        if not run_cmd(cmd):
+        cmd_out = run_cmd(cmd)
+        if cmd_out is None:
             return False
+
         # created the tar file, so now set the source to the tar file 
         ii["source"] = os.path.join(tar_dir, ii["tarFileName"])
 
         cmd = f"tar tf {ii['source']} | wc -l"
+
         output = run_cmd(cmd)
         if output is None:
             return False
+
         logging.verbose(f"got output: {output}")
         ii["num_files"] = int(output.stdout)
     else:
